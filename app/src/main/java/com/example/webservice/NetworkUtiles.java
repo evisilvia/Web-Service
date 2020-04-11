d6af53a5bff048d156d4089089142c65f66b8e50
evisilvia <evi_173040050@mail.unpas.ac.id>
2020-04-11 22:19:26 +0700

    mmmm

---
+++ app/src/main/java/com/example/webservice/NetworkUtiles.java
@@ -0,0 +1,13 @@
+package com.example.webservice;
+
+import android.app.DownloadManager;
+
+public class NetworkUtiles {
+    private static final String LOG_TAG = NetworkUtiles.class.getSimpleName();
+    private static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes?";
+    private static final String QUERY_PARAM = "q";
+    private static final String MAX_RESULT = "maxResults";
+    private static final String PRINT_TYPE = "pritType";
+
+    
+}
