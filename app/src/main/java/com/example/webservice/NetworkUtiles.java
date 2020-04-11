be5a7e1dc87790f1cca87c2a92d1beb80fc290cf
evisilvia <evi_173040050@mail.unpas.ac.id>
2020-04-11 22:53:49 +0700

    membuat file fetchbook dan NetworkUtiles

--- app/src/main/java/com/example/webservice/NetworkUtiles.java
+++ app/src/main/java/com/example/webservice/NetworkUtiles.java
@@ -2,12 +2,24 @@ package com.example.webservice;
 
 import android.app.DownloadManager;
 
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+
 public class NetworkUtiles {
     private static final String LOG_TAG = NetworkUtiles.class.getSimpleName();
     private static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes?";
     private static final String QUERY_PARAM = "q";
     private static final String MAX_RESULT = "maxResults";
     private static final String PRINT_TYPE = "pritType";
+
+
+//    static String getMahasiswa(String queryString){
+//        HttpURLConnection urlConnection = null;
+//        BufferedReader reader = null;
+//    }
+        
+
+
 
 
 }
